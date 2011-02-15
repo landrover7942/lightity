8c97f7da34f026a46899269ed67497592d4d1838
10koba01@gmail.com <10koba01@gmail.com@a9a6f5e3-e0f9-c4c6-1acb-9c2e89d8bba5>
2011-02-15 10:46:49 +0000

    [maven-release-plugin]  copy for tag lightity-0.3-beta-3

---
+++ tags/lightity-0.3-beta-3/src/main/java/com/google/code/lightity/NoSuchEntityPropertyException.java
@@ -0,0 +1,34 @@
+package com.google.code.lightity;
+
+/**
+ * Thrown to indicate that a given property has not been found.
+ * 
+ * @since 0.2
+ * @author Koba, Masafumi
+ */
+@SuppressWarnings("serial")
+public final class NoSuchEntityPropertyException extends RuntimeException {
+
+    private EntityProperty<?> property;
+
+    /**
+     * Constructor.
+     * 
+     * @param property
+     *            a property has not been found
+     */
+    public NoSuchEntityPropertyException(final EntityProperty<?> property) {
+        super(String.format("No such property <%s:%s>", property.getName(),
+                property.getType()));
+        this.property = property;
+    }
+
+    /**
+     * Gets property.
+     * 
+     * @return property
+     */
+    public EntityProperty<?> getProperty() {
+        return property;
+    }
+}
