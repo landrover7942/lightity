5c494937223c15d005211e773ea012c9ebe96fd8
10koba01@gmail.com <10koba01@gmail.com@a9a6f5e3-e0f9-c4c6-1acb-9c2e89d8bba5>
2011-02-15 10:09:26 +0000

    [maven-release-plugin]  copy for tag lightity-0.3-beta-2

---
+++ tags/lightity-0.3-beta-2/src/main/java/com/google/code/lightity/filter/GreaterThanFilter.java
@@ -0,0 +1,31 @@
+package com.google.code.lightity.filter;
+
+import com.google.code.lightity.Entity;
+import com.google.code.lightity.EntityProperty;
+
+final class GreaterThanFilter<T extends Comparable<T>> extends SimpleFilter<T> {
+
+    private static final String SYMBOL = ">";
+
+    private final EqualFilter<T> equalOperator;
+
+    public GreaterThanFilter(final EntityProperty<T> property, final T operand) {
+        super(property, operand, SYMBOL);
+        this.equalOperator = new EqualFilter<T>(property, operand);
+    }
+
+    @Override
+    public boolean apply(final Entity entity) {
+        if (equalOperator.apply(entity)) {
+            return false;
+        }
+        if (operand == null) {
+            return false;
+        }
+        final T get = entity.get(property);
+        if (get == null) {
+            return false;
+        }
+        return get.compareTo(operand) > 0;
+    }
+}
---
+++ tags/lightity-0.3-beta-2/src/test/java/com/google/code/lightity/filter/FilterTest.java
@@ -0,0 +1,127 @@
+package com.google.code.lightity.filter;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.code.lightity.EntityFactory;
+import com.google.code.lightity.EntityList;
+import com.google.code.lightity.EntityList.Filter;
+import com.google.code.lightity.EntityListFactory;
+import com.google.code.lightity.Person;
+
+public class FilterTest {
+
+    private EntityList persons;
+
+    private EntityList filtered;
+
+    @SuppressWarnings("boxing")
+    @Before
+    public void setUp() {
+        persons = EntityListFactory.create();
+        persons.add(EntityFactory.create().set(Person.NAME, "a")
+                .set(Person.AGE, 30));
+        persons.add(EntityFactory.create().set(Person.NAME, "b")
+                .set(Person.AGE, 20));
+        persons.add(EntityFactory.create().set(Person.NAME, "c")
+                .set(Person.AGE, 10));
+        filtered = EntityListFactory.create();
+    }
+
+    @Test
+    public void equal() {
+        filter(Filters.equal(Person.NAME, "c"));
+        assertEquals(1, filtered.size());
+        assertEquals("c", filtered.get(0).get(Person.NAME));
+    }
+
+    @SuppressWarnings("boxing")
+    @Test
+    public void greaterThan() {
+        filter(Filters.greaterThan(Person.AGE, 10));
+        assertEquals(2, filtered.size());
+        assertEquals("a", filtered.get(0).get(Person.NAME));
+        assertEquals("b", filtered.get(1).get(Person.NAME));
+    }
+
+    @SuppressWarnings("boxing")
+    @Test
+    public void greaterThanOrEqual() {
+        filter(Filters.greaterThanOrEqual(Person.AGE, 10));
+        assertEquals(3, filtered.size());
+        assertEquals("a", filtered.get(0).get(Person.NAME));
+        assertEquals("b", filtered.get(1).get(Person.NAME));
+        assertEquals("c", filtered.get(2).get(Person.NAME));
+    }
+
+    @SuppressWarnings("boxing")
+    @Test
+    public void lessThan() {
+        filter(Filters.lessThan(Person.AGE, 20));
+        assertEquals(1, filtered.size());
+        assertEquals("c", filtered.get(0).get(Person.NAME));
+    }
+
+    @SuppressWarnings("boxing")
+    @Test
+    public void lessThanOrEqual() {
+        filter(Filters.lessThanOrEqual(Person.AGE, 20));
+        assertEquals(2, filtered.size());
+        assertEquals("b", filtered.get(0).get(Person.NAME));
+        assertEquals("c", filtered.get(1).get(Person.NAME));
+    }
+
+    @Test
+    public void not() {
+        filter(Filters.not(Filters.equal(Person.NAME, "c")));
+        assertEquals(2, filtered.size());
+        assertEquals("a", filtered.get(0).get(Person.NAME));
+        assertEquals("b", filtered.get(1).get(Person.NAME));
+    }
+
+    @SuppressWarnings("boxing")
+    @Test
+    public void and() {
+        filter(Filters.and(Filters.equal(Person.NAME, "a"),
+                Filters.greaterThan(Person.AGE, 20)));
+        assertEquals(1, filtered.size());
+        assertEquals("a", filtered.get(0).get(Person.NAME));
+    }
+
+    @SuppressWarnings("boxing")
+    @Test
+    public void or() {
+        filter(Filters.or(Filters.equal(Person.AGE, 20),
+                Filters.greaterThan(Person.AGE, 20)));
+        assertEquals(2, filtered.size());
+        assertEquals("a", filtered.get(0).get(Person.NAME));
+        assertEquals("b", filtered.get(1).get(Person.NAME));
+    }
+
+    @Test
+    public void isNull() {
+        filter(Filters.isNull(Person.AGE));
+        assertEquals(0, filtered.size());
+    }
+
+    @Test
+    public void isNotNull() {
+        filter(Filters.isNotNull(Person.AGE));
+        assertEquals(persons, filtered);
+    }
+
+    @Test
+    public void in() {
+        filter(Filters.in(Person.NAME, "a", "c"));
+        assertEquals(2, filtered.size());
+        assertEquals("a", filtered.get(0).get(Person.NAME));
+        assertEquals("c", filtered.get(1).get(Person.NAME));
+    }
+
+    private void filter(Filter filter) {
+        System.out.println(filter);
+        filtered = persons.filter(filter);
+    }
+}
