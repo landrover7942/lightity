b0f215cb1fc03a03c451604cea0e66afd53ed7df
10koba01@gmail.com <10koba01@gmail.com@a9a6f5e3-e0f9-c4c6-1acb-9c2e89d8bba5>
2011-02-15 05:54:54 +0000

    Organize import.

--- trunk/src/test/java/com/google/code/lightity/filter/FilterTest.java
+++ trunk/src/test/java/com/google/code/lightity/filter/FilterTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.google.code.lightity.Entity;
 import com.google.code.lightity.EntityFactory;
 import com.google.code.lightity.EntityList;
 import com.google.code.lightity.EntityList.Filter;
