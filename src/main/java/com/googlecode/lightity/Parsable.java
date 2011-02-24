962120b8e2de27a2a5722a6a0dfdfb601176d36e
1 <10koba01@gmail.com>
2011-02-24 10:55:03 +0000

    Modify API documentations.

--- src/main/java/com/googlecode/lightity/Parsable.java
+++ src/main/java/com/googlecode/lightity/Parsable.java
@@ -1,5 +1,20 @@
 package com.googlecode.lightity;
 
+/**
+ * An object which can be parsed.
+ * 
+ * @param <T>
+ *            a type of parsed result object
+ * @author Koba, Masafumi
+ */
 public interface Parsable<T> {
+
+    /**
+     * Parse a given source.
+     * 
+     * @param source
+     *            a char sequence(almost string)
+     * @return an object created by parsing
+     */
     T parse(CharSequence source);
 }
--- src/main/java/com/googlecode/lightity/Validatable.java
+++ src/main/java/com/googlecode/lightity/Validatable.java
@@ -2,6 +2,22 @@ package com.googlecode.lightity;
 
 import java.util.List;
 
+/**
+ * An object which can be validated.
+ * 
+ * @param <T>
+ *            a type of validated object
+ * @author Koba, Masafumi
+ */
 public interface Validatable<T> {
+
+    /**
+     * Validate an given target.
+     * 
+     * @param target
+     *            an object to be validated
+     * @return a list of validation error messages. An empty list if the
+     *         validation is successful.
+     */
     List<String> validate(T target);
 }
