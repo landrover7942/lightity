10a27070e2a5bd1f2d2077a833e92d7f5e50cdf1
1 <10koba01@gmail.com>
2011-02-25 15:41:00 +0000

    Add jsr-305 dependency, and classify the immutability of EntityProperty.

--- src/main/java/com/googlecode/lightity/EntityProperty.java
+++ src/main/java/com/googlecode/lightity/EntityProperty.java
@@ -1,5 +1,8 @@
 package com.googlecode.lightity;
 
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+
 /**
  * Property of {@link Entity}.
  * 
@@ -7,6 +10,7 @@ package com.googlecode.lightity;
  *            a type of property
  * @author Koba, Masafumi
  */
+@Immutable
 public interface EntityProperty<T> {
 
     /**
@@ -14,6 +18,7 @@ public interface EntityProperty<T> {
      * 
      * @return a name of this property
      */
+    @Nonnull
     String getName();
 
     /**
@@ -21,5 +26,20 @@ public interface EntityProperty<T> {
      * 
      * @return a type class of this property
      */
+    @Nonnull
     Class<T> getType();
+
+    /**
+     * Use return values of {@link #getName()} and {@link #getType()}.
+     * {@inheritDoc}
+     */
+    @Override
+    boolean equals(Object obj);
+
+    /**
+     * Use return values of {@link #getName()} and {@link #getType()}.
+     * {@inheritDoc}
+     */
+    @Override
+    int hashCode();
 }
