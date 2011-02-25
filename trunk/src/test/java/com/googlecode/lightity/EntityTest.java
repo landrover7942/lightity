67bab4db4de2c476ece0c264ecbeae75c8504463
10koba01@gmail.com <10koba01@gmail.com@a9a6f5e3-e0f9-c4c6-1acb-9c2e89d8bba5>
2011-02-25 16:14:15 +0000

    Add immutable implementation of Entity.

--- trunk/src/test/java/com/googlecode/lightity/EntityTest.java
+++ trunk/src/test/java/com/googlecode/lightity/EntityTest.java
@@ -15,65 +15,115 @@ import org.junit.matchers.JUnitMatchers;
 
 public class EntityTest {
 
-    private Entity person;
+    private Entity mutable;
+
+    private Entity immutable;
 
     @Before
     public void setUp() {
-        person = EntityFactory.create();
+        mutable = EntityFactory.mutable();
+        immutable = EntityFactory.immutable();
     }
 
     @SuppressWarnings("boxing")
     @Test
-    public void whenNormalScenario() throws Exception {
-        assertThat(person.set(Person.NAME, "abc"), notNullValue());
-        assertThat(person.count(), is(1));
-        assertThat(person.exists(Person.NAME), is(true));
-        assertThat(person.get(Person.NAME), is("abc"));
-        assertThat(person, has(Person.NAME));
+    public void whenMutableScenario() throws Exception {
+        assertThat(mutable.set(Person.NAME, "abc"), notNullValue());
+        assertThat(mutable.count(), is(1));
+        assertThat(mutable.exists(Person.NAME), is(true));
+        assertThat(mutable.get(Person.NAME), is("abc"));
+        assertThat(mutable, has(Person.NAME));
 
-        assertThat(person.set(Person.AGE, 10), notNullValue());
-        assertThat(person.count(), is(2));
-        assertThat(person.exists(Person.AGE), is(true));
-        assertThat(person.get(Person.AGE), is(10));
-        assertThat(person, has(Person.AGE));
+        assertThat(mutable.set(Person.AGE, 10), notNullValue());
+        assertThat(mutable.count(), is(2));
+        assertThat(mutable.exists(Person.AGE), is(true));
+        assertThat(mutable.get(Person.AGE), is(10));
+        assertThat(mutable, has(Person.AGE));
 
-        assertThat(person.delete(Person.NAME), notNullValue());
-        assertThat(person.count(), is(1));
-        assertThat(person.exists(Person.NAME), is(false));
-        assertThat(person, not(has(Person.NAME)));
+        assertThat(mutable.delete(Person.NAME), notNullValue());
+        assertThat(mutable.count(), is(1));
+        assertThat(mutable.exists(Person.NAME), is(false));
+        assertThat(mutable, not(has(Person.NAME)));
 
-        assertThat(person.delete(Person.AGE), notNullValue());
-        assertThat(person.count(), is(0));
-        assertThat(person.exists(Person.AGE), is(false));
-        assertThat(person, not(has(Person.AGE)));
+        assertThat(mutable.delete(Person.AGE), notNullValue());
+        assertThat(mutable.count(), is(0));
+        assertThat(mutable.exists(Person.AGE), is(false));
+        assertThat(mutable, not(has(Person.AGE)));
+    }
+
+    @SuppressWarnings("boxing")
+    @Test
+    public void whenImmutableScenario() throws Exception {
+        immutable = immutable.set(Person.NAME, "abc");
+        assertThat(immutable, notNullValue());
+        assertThat(immutable.count(), is(1));
+        assertThat(immutable.exists(Person.NAME), is(true));
+        assertThat(immutable.get(Person.NAME), is("abc"));
+        assertThat(immutable, has(Person.NAME));
+
+        immutable = immutable.set(Person.AGE, 10);
+        assertThat(immutable, notNullValue());
+        assertThat(immutable.count(), is(2));
+        assertThat(immutable.exists(Person.AGE), is(true));
+        assertThat(immutable.get(Person.AGE), is(10));
+        assertThat(immutable, has(Person.AGE));
+
+        immutable = immutable.delete(Person.NAME);
+        assertThat(immutable, notNullValue());
+        assertThat(immutable.count(), is(1));
+        assertThat(immutable.exists(Person.NAME), is(false));
+        assertThat(immutable, not(has(Person.NAME)));
+
+        immutable = immutable.delete(Person.AGE);
+        assertThat(immutable, notNullValue());
+        assertThat(immutable.count(), is(0));
+        assertThat(immutable.exists(Person.AGE), is(false));
+        assertThat(immutable, not(has(Person.AGE)));
     }
 
     @SuppressWarnings("boxing")
     @Test
     public void whenEntityIsEmpty() throws Exception {
-        assertThat(person.count(), is(0));
-        assertThat(person.exists(Person.NAME), is(false));
-        assertThat(person.exists(Person.AGE), is(false));
-        assertThat(person, has());
+        assertThat(mutable.count(), is(0));
+        assertThat(mutable.exists(Person.NAME), is(false));
+        assertThat(mutable.exists(Person.AGE), is(false));
+        assertThat(mutable, has());
+
+        assertThat(immutable.count(), is(0));
+        assertThat(immutable.exists(Person.NAME), is(false));
+        assertThat(immutable.exists(Person.AGE), is(false));
+        assertThat(immutable, has());
     }
 
     @Test(expected = NoSuchEntityPropertyException.class)
-    public void whenTheGivenPropertyIsNotFound() throws Exception {
-        person.get(Person.NAME);
+    public void whenTheGivenPropertyIsNotFoundByMutable() throws Exception {
+        mutable.get(Person.NAME);
+    }
+
+    @Test(expected = NoSuchEntityPropertyException.class)
+    public void whenTheGivenPropertyIsNotFoundByImmutable() throws Exception {
+        immutable.get(Person.NAME);
     }
 
     @Test
     public void whenNonexistPropertyIsDeleted() throws Exception {
-        assertThat("not thrown exception", person.delete(Person.NAME),
+        assertThat("not thrown exception", mutable.delete(Person.NAME),
+                notNullValue());
+
+        assertThat("not thrown exception", immutable.delete(Person.NAME),
                 notNullValue());
     }
 
     @SuppressWarnings("boxing")
     @Test
     public void whenEntityIsConvertedToMap() throws Exception {
-        assertThat(person.toMap().size(), is(0));
-        assertThat(person.set(Person.NAME, "Tom").set(Person.AGE, 20).toMap(),
+        assertThat(mutable.toMap().size(), is(0));
+        assertThat(mutable.set(Person.NAME, "Tom").set(Person.AGE, 20).toMap(),
                 isMap(Person.NAME, "Tom", Person.AGE, 20));
+
+        assertThat(immutable.toMap().size(), is(0));
+        assertThat(immutable.set(Person.NAME, "Tom").set(Person.AGE, 20)
+                .toMap(), isMap(Person.NAME, "Tom", Person.AGE, 20));
     }
 
     private static Matcher<Iterable<EntityProperty<?>>> has(
@@ -81,8 +131,9 @@ public class EntityTest {
         return JUnitMatchers.<EntityProperty<?>> hasItems(properties);
     }
 
-    private static Matcher<Map<String, Object>> isMap(EntityProperty<?> p1,
-            Object v1, EntityProperty<?> p2, Object v2) {
+    private static Matcher<Map<String, Object>> isMap(
+            final EntityProperty<?> p1, final Object v1,
+            final EntityProperty<?> p2, final Object v2) {
         final Map<String, Object> map = new HashMap<String, Object>(2);
         map.put(p1.getName(), v1);
         map.put(p2.getName(), v2);
