792b4b8f22ce32d1b54f8e64d89d7f1b62d3fd18
10koba01@gmail.com <10koba01@gmail.com@a9a6f5e3-e0f9-c4c6-1acb-9c2e89d8bba5>
2011-03-10 10:07:48 +0000

    Rollback version 0.5 commit.

--- tags/lightity-0.5/src/main/java/com/googlecode/lightity/filter/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Implementations of {@link com.googlecode.lightity.EntityList.Filter}.
- * <p>
- * <strong>This package is deprecated. It will be removed on a future version.</strong>
- */
-package com.googlecode.lightity.filter;
-
