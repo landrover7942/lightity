0ab08a484f9dba1cfb76a39fd1e3ee1324a4d88f
10koba01@gmail.com <10koba01@gmail.com@a9a6f5e3-e0f9-c4c6-1acb-9c2e89d8bba5>
2011-03-10 08:49:34 +0000

    No commit javadoc.

--- tags/lightity-0.5/src/test/java/com/googlecode/lightity/Person.java
+++
@@ -1,8 +0,0 @@
-package com.googlecode.lightity;
-
-public interface Person {
-    EntityProperty<String> NAME = EntityPropertyFactory.create("name",
-            String.class);
-    EntityProperty<Integer> AGE = EntityPropertyFactory.create("age",
-            Integer.class);
-}
